f851626c0f32e5012da9880f9cf241ba936ce02e
vuthanhdatt <80867452+vuthanhdatt@users.noreply.github.com>
2021-12-07 22:34:17 +0700

    Only update data on Github

--- main.py
+++ main.py
@@ -13,32 +13,32 @@ from datetime import date
 # load environment variables
 load_dotenv(find_dotenv())
 
-SHEET_KEY = os.getenv('SHEET_KEY')
-service_account_info = ast.literal_eval(SHEET_KEY)
+# SHEET_KEY = os.getenv('SHEET_KEY')
+# service_account_info = ast.literal_eval(SHEET_KEY)
 start_date = '2000-01-01'
 today = date.today().strftime("%Y-%m-%d")
 
-scopes = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-]
+# scopes = [
+#     'https://www.googleapis.com/auth/spreadsheets',
+#     'https://www.googleapis.com/auth/drive'
+# ]
 
-credentials = Credentials.from_service_account_info(
-    service_account_info,
-    scopes=scopes
-)
+# credentials = Credentials.from_service_account_info(
+#     service_account_info,
+#     scopes=scopes
+# )
 
-gc = gspread.authorize(credentials)
+# gc = gspread.authorize(credentials)
 
 def auto(list_com, exchange, sheet_id):
-    sh = gc.open_by_key(sheet_id)
+    # sh = gc.open_by_key(sheet_id)
     for com in list_com:
         df = get_price_history(com, start_date, today)
-        worksheet = sh.add_worksheet(title=com, rows="100", cols="4")
-        worksheet = sh.worksheet(com)
-        set_with_dataframe(worksheet, df, include_index=True)
+        # worksheet = sh.add_worksheet(title=com, rows="100", cols="4")
+        # worksheet = sh.worksheet(com)
+        # set_with_dataframe(worksheet, df, include_index=True)
         df.to_csv(f'{exchange}/{com}.csv')
-        time.sleep(1.8)
+        # time.sleep(1.8)
 
 
 if __name__ == "__main__":
