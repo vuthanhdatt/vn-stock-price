701e8dd9de56cdcf3c921c7999afa9884de59144
vuthanhdatt <80867452+vuthanhdatt@users.noreply.github.com>
2021-12-09 21:30:13 +0700

    Restructure

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import ast
 import logging
 import os
-import time
 from datetime import date
 import asyncio
 import gspread_asyncio
 
-import gspread
 from dotenv import find_dotenv, load_dotenv
 from google.oauth2.service_account import Credentials
-from gspread_dataframe import set_with_dataframe
 
 from get_com import get_all_com
 from get_price import get_price_history
@@ -22,7 +19,7 @@ service_account_info = ast.literal_eval(SHEET_KEY)
 start_date = '2000-01-01'
 today = date.today().strftime("%Y-%m-%d")
 
-#logging
+########### FOR LOGGING ##############
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
@@ -30,6 +27,7 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s","%d-%m
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+########### FOR AUTHENTIC #############
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
@@ -39,24 +37,26 @@ credentials = Credentials.from_service_account_info(
     service_account_info,
     scopes=scopes
 )
+########### SYNCHRONOUS FUNCTION ############
+#REMOVE
 
-gc = gspread.authorize(credentials)
+# gc = gspread.authorize(credentials)
 
-def auto(list_com, exchange, sheet_id):
-    sh = gc.open_by_key(sheet_id)
-    i = -4
-    e = 2.718281
-    for com in list_com:
-        df = get_price_history(com, start_date, today)
-        worksheet = sh.add_worksheet(title=com, rows="100", cols="4")
-        worksheet = sh.worksheet(com)
-        set_with_dataframe(worksheet, df, include_index=True)
-        df.to_csv(f'{exchange}/{com}.csv')
-        i += 0.01
-        t = 1.8 - e**i
-        if t>0:
-            time.sleep(t)
-        logger.info(f"{com} DONE!")
+# def auto(list_com, exchange, sheet_id):
+#     sh = gc.open_by_key(sheet_id)
+#     i = -4
+#     e = 2.718281
+#     for com in list_com:
+#         df = get_price_history(com, start_date, today)
+#         worksheet = sh.add_worksheet(title=com, rows="100", cols="4")
+#         worksheet = sh.worksheet(com)
+#         set_with_dataframe(worksheet, df, include_index=True)
+#         df.to_csv(f'{exchange}/{com}.csv')
+#         i += 0.01
+#         t = 1.8 - e**i
+#         if t>0:
+#             time.sleep(t)
+#         logger.info(f"{com} DONE!")
 
 
 def get_credendital():
@@ -66,22 +66,27 @@ def get_credendital():
 )
     return credentials
 
-
+############# FOR USING UPDATE ON GITHUB ############
 async def github(exchange, com_list):
-    D = {}
+    '''
+    Async requests to data source to upload to Github, return dict of DataFrame
+    to use in gsheet requests
+    '''
+    df_dict = {}
     semaphore = asyncio.Semaphore(10)
     async def single_file(com):
         async with semaphore:
-            logger.info(f'{com} DONE!')
+            logger.info(f'{com, exchange} DONE!')
             result = await get_price_history(com,start_date,today)
             result.to_csv(f'{exchange}/{com}.csv')
-            D[com] = result
+            df_dict[com] = result
 
     coros = [single_file(com) for com in com_list]
     await asyncio.gather(*coros)
-    return D
+    return df_dict
 
 ######### FOR USING BATCH_UPDATE #############   
+#CURRENTLY NOT USING
 # def value(df):
 #     row = df.shape[0] +1
 #     l = []
@@ -92,24 +97,27 @@ async def github(exchange, com_list):
 #         l.append(d)
 #     return l
 
-async def sheet(agcm, sheet_id,com_list,D):
+######## FOR UPDATING ON GOOGLE SHEET #######
+async def sheet(agcm, sheet_id,com_list,df_dict):
     agc = await agcm.authorize()
     ss = await agc.open_by_key(sheet_id)
 
     async def single_sheet(com):
         # df = get_price_history(com, start_date, today)
-        df = D[com]
+        df = df_dict[com]
         # value_update = value(df)
         row = df.shape[0] +1
         # ws = await ss.add_worksheet(title=com, rows="100", cols="7")
         try:
-            logger.info(f'SHEET AWAIT {com}')
             ws = await ss.worksheet(com)
+            logger.info(f'SHEET AWAIT {com}')
         except:
+            ws = await ss.add_worksheet(title=com, rows="1000", cols="7")
             logger.info(f'SHEET ADD {com}')
-            ws = await ss.add_worksheet(title=com, rows="100", cols="7")
 
         await ws.update(f'A1:H{row}',[df.columns.values.tolist()] + df.values.tolist())
+        # print(value_update)
+        # await ws.batch_update(value_update)
         logger.info(f"{com} SHEET DONE!")
 
     coros = [single_sheet(com) for com in com_list]
@@ -127,27 +135,14 @@ if __name__ == "__main__":
     HOSE_SHEET_ID = '1Br0SphvPJH5PZ0JSFtZk24dHUsR17uxIM4s38GBCAA4'
     HNX_SHEET_ID = '1wM8UK3UbDGQJk_TkF292vYSe2OC4chxLTHVmta9D16A'
     UPCOM_SHEET_ID = '1WAHZEe6Hgzua7izI9T3wFK7Rre1KSZVQKIG9sHGzYis'
-    TEST_SHEET_ID = '1KykDw2GYpiCuJluz_TC06nECDuzHjEI5hGSmb7VSxW8'
-
-    # auto(hose_com, 'hose', HOSE_SHEET_ID)
-    # # # # auto(hnx_com, 'hnx', HNX_SHEET_ID)
-    # # # # auto(upcom_com, 'upcom', UPCOM_SHEET_ID)
-    # # # sh = gc.open_by_key(TEST_SHEET_ID)
-
-    # # df = get_price_history('VCB', start_date, today)
-    # # # worksheet = sh.add_worksheet(title='TEST', rows="100", cols="4")
-    # # # worksheet = sh.worksheet('TEST')
-    # # # worksheet.update([[42,43], [43,45]])
-    # # # print([df.columns.values.tolist()])
 
     agcm = gspread_asyncio.AsyncioGspreadClientManager(get_credendital)
     loop = asyncio.get_event_loop()
 
-    hose = loop.run_until_complete(github('hose',hose_com))
-    hnx = loop.run_until_complete(github('hnx',hnx_com))
-    upcom = loop.run_until_complete(github('upcom',upcom_com))
+    hose_df_dict = loop.run_until_complete(github('hose',hose_com))
+    hnx_df_dict = loop.run_until_complete(github('hnx',hnx_com))
+    upcom_df_dict = loop.run_until_complete(github('upcom',upcom_com))
 
-    loop.run_until_complete(sheet(agcm,HOSE_SHEET_ID,hose_com,hose))
-    loop.run_until_complete(sheet(agcm,HNX_SHEET_ID,hnx_com,hnx))
-    loop.run_until_complete(sheet(agcm,UPCOM_SHEET_ID,upcom_com,upcom))
-    
+    loop.run_until_complete(sheet(agcm,HOSE_SHEET_ID,hose_com,hose_df_dict))
+    loop.run_until_complete(sheet(agcm,HNX_SHEET_ID,hnx_com,hnx_df_dict))
+    loop.run_until_complete(sheet(agcm,UPCOM_SHEET_ID,upcom_com,upcom_df_dict))
